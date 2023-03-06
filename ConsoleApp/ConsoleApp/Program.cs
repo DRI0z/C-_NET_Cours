d8ef96a42c4f41ab1000672cae33c31b86cb84bf
EnzoVOISIN <enzo.voisin@devinfluence.fr>
2023-03-06 13:51:42 +0100

    Taxe, SalaireBrut et calcul du net mensuel et annuel

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,22 +3,58 @@
 
     static void Taxes(double anualGrossSalary, double tax)
     {
-        double anualNetSalary = anualGrossSalary - (anualGrossSalary * (tax / 100));
-        double netSalary = anualNetSalary / 12;
+        double anualNetSalary = Math.Round(anualGrossSalary - (anualGrossSalary * (tax / 100)), 2);
+        double netSalary = Math.Round((anualNetSalary / 12), 2);
 
         Console.WriteLine("Le salaire Brut annuel : " + anualGrossSalary + "€, le salaire Net annuel : " + anualNetSalary + "€, le salaire Net mensuel :" + netSalary + "€");
+
+        if (anualGrossSalary >= 50000.0)
+        {
+            Console.WriteLine("Conseille d'ami, fais des dons pour réduire les impôts");
+        }
+        else if (netSalary <= 1501.0)
+        {
+            Console.WriteLine("C'est normal c'est le salaire d'un alternant");
+        }
+        else if(anualGrossSalary >= 30000.0 && anualGrossSalary <= 40000.0)
+        {
+            Console.WriteLine("Faut faire un bac +5 en dev");
+        }
+
     }
 
 
     static void Main(string[] args)
     {
+        
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+        double anualGrossSalary;
+        double tax;
+
         Console.WriteLine("Le salaire annuel brut : ");
-        double anualGrossSalary = double.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        if(double.TryParse(input, out anualGrossSalary))
+        {
+            Console.WriteLine("Vous avez entré : "+anualGrossSalary+"€");
+        }
+        else
+        {
+            Console.WriteLine("Vous n'avez pas rentré un nombre");
+        }
 
         Console.WriteLine("La taxe : ");
-        double tax = double.Parse(Console.ReadLine());
+        input = Console.ReadLine();
+
+        if (double.TryParse(input, out tax))
+        {
+            Console.WriteLine("Vous avez entré : " + tax + "%");
+        }
+        else
+        {
+            Console.WriteLine("Vous n'avez pas rentré un poucentage");
+        }
 
         Taxes(anualGrossSalary, tax);
     }
