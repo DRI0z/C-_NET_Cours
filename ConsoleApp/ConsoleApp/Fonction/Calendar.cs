f0345a66417bd0770f6a079f3329487075f426cf
EnzoVOISIN <enzo.voisin@devinfluence.fr>
2023-03-06 15:41:08 +0100

    SalaryMonth

---
+++ ConsoleApp/ConsoleApp/Fonction/Calendar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Fonction
+{
+    public class Calendar
+    {
+        public static string[] monthCalendar = new string[] { "Janaruy", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December", "Prime"};
+
+
+        public static void SalaryMonth(double netMonthlySalary, double anualNetSalary)
+        {
+            for(int i = 0; i < monthCalendar.Length; i++)
+            {
+                if (monthCalendar[i] == "August")
+                {
+                    Console.WriteLine(monthCalendar[i] + " : Vacances");
+                }
+                else if (monthCalendar[i] == "Prime")
+                {
+                    Console.WriteLine(monthCalendar[i] +" de fin d'année : "+ Math.Round(anualNetSalary * 0.1 ,2)+"€");
+                }
+                else
+                { 
+                    Console.WriteLine(monthCalendar[i]+ " : " +netMonthlySalary);
+                }
+            }
+        }
+
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,41 +1,7 @@
-﻿class Program
+﻿using ConsoleApp.Fonction;
+
+class Program
 {
-
-    static void MessageInfo(double anualGrossSalary, double anualNetSalary, double netMonthlySalary)
-    {
-        switch(true)
-        {
-            case bool condition0 when anualGrossSalary >= 50000.0:
-                Console.WriteLine("Conseille d'ami, fais des dons pour réduire les impôts");
-                break;
-
-            case bool condition1 when anualNetSalary >= 30000.0 || anualNetSalary <= 40000.0:
-                Console.WriteLine("Faut faire un Bac+5 en Dév");
-                break;
-
-            case bool condition2 when netMonthlySalary < 1501.0:
-                Console.WriteLine("C'est normal c'est le salaire d'un alternant");
-                break;
-
-            default:
-                Console.WriteLine("Aucune condition n'est remplie");
-                break;
-
-        }
-    }
-    
-    
-    static void Taxes(double anualGrossSalary, double tax)
-    {
-        double anualNetSalary = Math.Round(anualGrossSalary - (anualGrossSalary * (tax / 100)), 2);
-        double netMonthlySalary = Math.Round((anualNetSalary / 12), 2);
-
-        Console.WriteLine("Le salaire Brut annuel : " + anualGrossSalary + "€, le salaire Net annuel : " + anualNetSalary + "€, le salaire Net mensuel :" + netMonthlySalary + "€");
-
-        MessageInfo(anualGrossSalary, anualNetSalary, netMonthlySalary);
-    }
-
-
     static void Main(string[] args)
     {
         
@@ -68,6 +34,6 @@
             Console.WriteLine("Vous n'avez pas rentré un poucentage");
         }
 
-        Taxes(anualGrossSalary, tax);
+        Taxes.Tax(anualGrossSalary, tax);
     }
 }
