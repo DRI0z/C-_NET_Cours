63e0b934c626276cc5319d575befba03c4b72d28
EnzoVOISIN <enzo.voisin@devinfluence.fr>
2023-03-06 12:10:05 +0100

    Taxe

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,8 +1,25 @@
 ﻿class Program
 {
+
+    static void Taxes(double anualGrossSalary, double tax)
+    {
+        double anualNetSalary = anualGrossSalary - (anualGrossSalary * (tax / 100));
+        double netSalary = anualNetSalary / 12;
+
+        Console.WriteLine("Le salaire Brut annuel : " + anualGrossSalary + "€, le salaire Net annuel : " + anualNetSalary + "€, le salaire Net mensuel :" + netSalary + "€");
+    }
+
+
     static void Main(string[] args)
     {
-        int number = 10;
-        Console.WriteLine("id : " + number);
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Le salaire annuel brut : ");
+        double anualGrossSalary = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("La taxe : ");
+        double tax = double.Parse(Console.ReadLine());
+
+        Taxes(anualGrossSalary, tax);
     }
 }
