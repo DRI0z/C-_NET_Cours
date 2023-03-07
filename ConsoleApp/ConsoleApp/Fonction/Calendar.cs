a9fb24651f9bab9fa87e3e216a569e9c8ea56018
EnzoVOISIN <enzo.voisin@devinfluence.fr>
2023-03-07 09:55:54 +0100

    Try Catch pour la prime

--- ConsoleApp/ConsoleApp/Fonction/Calendar.cs
+++ ConsoleApp/ConsoleApp/Fonction/Calendar.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp.Fonction
         public static string[] monthCalendar = new string[] { "Janaruy", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December", "Prime"};
 
-
         public static void SalaryMonth(double netMonthlySalary, double anualNetSalary)
         {
             for(int i = 0; i < monthCalendar.Length; i++)
@@ -22,7 +21,27 @@ namespace ConsoleApp.Fonction
                 }
                 else if (monthCalendar[i] == "Prime")
                 {
-                    Console.WriteLine(monthCalendar[i] +" de fin d'année : "+ Math.Round(anualNetSalary * 0.1 ,2)+"€");
+                    try
+                    {
+                        Console.WriteLine("Mettre le montant en % de la prime");
+                        double primeValue = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Mettre le montant de division de la prime");
+                        int division = int.Parse((Console.ReadLine()));
+
+                        primeValue /= division;
+                        Console.WriteLine(monthCalendar[i] + " de fin d'année : " + Math.Round(anualNetSalary * primeValue, 2) + "€");
+                    }
+                    catch(FormatException e)
+                    {
+                        Console.WriteLine("Le type de caractère que vous venez de saisir n'est pas pris en charge");
+                        Environment.Exit(1);
+                    }
+                    catch(DivideByZeroException e)
+                    {
+                        Console.WriteLine("Le pourcentage de prime ne peut être égale à 0%"); 
+                        Environment.Exit(1);
+                    }
                 }
                 else
                 { 
