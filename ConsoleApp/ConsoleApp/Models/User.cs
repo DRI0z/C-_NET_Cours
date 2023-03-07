370159dd40344721fb697da45361aa806f345a42
EnzoVOISIN <enzo.voisin@devinfluence.fr>
2023-03-07 12:29:30 +0100

    User Constructor

--- ConsoleApp/ConsoleApp/Models/User.cs
+++ ConsoleApp/ConsoleApp/Models/User.cs
@@ -9,5 +9,14 @@
         public double AnnualSalaryBrut { get; set; }
         public double Tax { get; set; }
 
+        public User (int id, string firstName, string lastName, int old, double annualSalaryBrut, double tax)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Old = old;
+            this.AnnualSalaryBrut = annualSalaryBrut;
+            this.Tax = tax;
+        }
     }
 }
